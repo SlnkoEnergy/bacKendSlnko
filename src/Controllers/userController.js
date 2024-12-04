1b70d7b76bc20c0b74beea5bbc93fe856ef55271
it@slnkoenergy.com <it@slnkoenergy.com>
2024-12-04 17:58:55 +0530

    neme

--- src/Controllers/userController.js
+++ src/Controllers/userController.js
@@ -57,16 +57,16 @@ const userRegister = async function (req, res) {
 
 const forgettpass = async function (req, res) {
   try {
-    const { email } = req.body;
+    const { name } = req.body;
 
-    if (!email) {
-      return res.status(400).json({ message: "Email is required" });
+    if (!name) {
+      return res.status(400).json({ message: "Invalid credential" });
     }
 
     const otp = Math.floor(100000 + Math.random() * 900000);
 
     const user = await userModells.findOneAndUpdate(
-      { email },
+      { name },
       { otp },
       { new: true }
     );
@@ -99,7 +99,7 @@ const forgettpass = async function (req, res) {
 
       res.status(200).json({
         message: "OTP sent successfully. Please check your email.",
-        email,
+        name,
         otp, // In production, avoid exposing the OTP in the response.
         userID: user._id,
       });
@@ -112,10 +112,10 @@ const forgettpass = async function (req, res) {
 
 const resetpassword = async function (req, res) {
     try {
-      const { email, otp, newpassword } = req.body;
+      const { name, otp, newpassword } = req.body;
   
       // Find the user by email and otp
-      const user = await userModells.findOne({ email: email, otp: otp }); // Use findOne for a single document
+      const user = await userModells.findOne({ name: name, otp: otp }); // Use findOne for a single document
   
       // Check if the user exists
       if (!user) {
