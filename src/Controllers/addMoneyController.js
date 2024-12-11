68f27db595e3c3d1d6b1ee12ba06915062b069d8
it@slnkoenergy.com <it@slnkoenergy.com>
2024-12-11 17:29:32 +0530

    NOT CREDIT

--- src/Controllers/addMoneyController.js
+++ src/Controllers/addMoneyController.js
@@ -81,7 +81,7 @@ const credit_amount = async function (req, res) {
     ]);
 
     // If no matching records found
-    if (credits) {
+    if (!credits) {
       return res.status(404).json({ message: "No credit history found" });
     }
 
