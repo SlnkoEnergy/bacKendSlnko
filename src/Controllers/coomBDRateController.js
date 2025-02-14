8f596efde6b7bdbf8be43f279aa986cb07449295
Biplove82 <biplavmandal.mandal@gmail.com>
2025-02-14 11:55:44 +0530

    COMMBDD

---
+++ src/Controllers/coomBDRateController.js
@@ -0,0 +1,115 @@
+const CommBDRate = require('../Modells/commBDRateModells');
+
+//add commBDRate
+const addCommBDRate = async function (req, res) {
+    const {
+        spv_modules_555,
+        spv_modules_580,
+        spv_modules_550,
+        spv_modules_585,
+        
+        solar_inverter,
+        module_mounting_structure_4LX6,
+        module_mounting_structure_2PX24,
+        module_mounting_structure_2PX12,
+        mounting_hardware,
+        dc_cable,
+        ac_cable_inverter_accb,
+        ac_cable_accb_transformer,
+        ac_ht_cable_11KV,
+        ac_ht_cable_33KV,
+        earthing_station,
+        earthing_strips,
+        earthing_strip,
+        lightening_arrestor,
+        datalogger,
+        auxilary_transformer,
+        ups_ldb,
+        balance_of_system,
+        transportation,
+        transmission_line_11kv,
+        transmission_line_33kv,
+       
+        ct_pt_11kv_MP,
+        ct_pt_33kv_MP,
+        ct_pt_11kv_Other,
+        ct_pt_33kv_Other,
+        abt_meter_11kv_MP,
+        abt_meter_33kv_MP,
+        abt_meter_11kv_Other,
+        abt_meter_33kv_Other,
+        vcb_kiosk,
+        slnko_charges,
+        installation_commissioing, 
+        submitted_by,  } = req.body;
+    try {
+        let commBDRate = new CommBDRate({
+            spv_modules_555,
+            spv_modules_580,
+            spv_modules_550,
+            spv_modules_585,
+        
+            solar_inverter,
+            module_mounting_structure_4LX6,
+            module_mounting_structure_2PX24,
+            module_mounting_structure_2PX12,
+            mounting_hardware,
+            dc_cable,
+            ac_cable_inverter_accb,
+            ac_cable_accb_transformer,
+            ac_ht_cable_11KV,
+            ac_ht_cable_33KV,
+            earthing_station,
+            earthing_strips,
+            earthing_strip,
+            lightening_arrestor,
+            datalogger,
+            auxilary_transformer,
+            ups_ldb,
+            balance_of_system,
+            transportation,
+            transmission_line_11kv,
+            transmission_line_33kv,
+           
+            ct_pt_11kv_MP,
+            ct_pt_33kv_MP,
+            ct_pt_11kv_Other,
+            ct_pt_33kv_Other,
+            abt_meter_11kv_MP,
+            abt_meter_33kv_MP,
+            abt_meter_11kv_Other,
+            abt_meter_33kv_Other,
+            vcb_kiosk,
+            slnko_charges,
+            installation_commissioing, 
+            submitted_by,
+        });
+        await commBDRate.save();
+        res.status(200).json({ msg: "Comm Rate added successfully", commBDRate : commBDRate});
+    } catch (error) {
+        return res.status(400).json({ msg: "Server error", error: error.message });
+    }
+};
+//get all commBDRate
+
+const getCommBDRate = async function (req, res) {
+    try {
+        const commBDRate = await CommBDRate.find();
+        return res.status(200).json(commBDRate);
+    } catch (error) {
+        return res.status(500).json({ msg: error.message });
+    }
+};
+//edit commBDRate
+const editCommBDRate = async function (req, res) {
+    try {
+        let { _id } = req.params;
+        let updateData = req.body;
+        let data = await CommBDRate.findByIdAndUpdate(_id, updateData, { new: true });  
+        if (!data) {
+            return res.status(404).json({ msg: "user not found" });
+        }
+    } catch (error) {
+        return res.status(500).json({ msg: error.message });
+    }
+}
---
+++ src/Modells/commBDRateModells.js
@@ -0,0 +1,56 @@
+const mongoose = require('mongoose');
+ const commBDRateSchema = new mongoose.Schema({
+    
+    spv_modules_555: { type: String,default:" " },
+    spv_modules_580: { type: String,default:" " },
+    spv_modules_550: { type: String,default:" " },
+    spv_modules_585: { type: String,default:" " },
+   // offer_id: { type: String,default:" " },
+    solar_inverter: { type: String },
+    module_mounting_structure_4LX6: { type: String,default:" " },
+    module_mounting_structure_2PX24: { type: String,default:" " },
+    module_mounting_structure_2PX12: { type: String,default:" " },
+    mounting_hardware: { type: String },
+    dc_cable: { type: String },
+    ac_cable_inverter_accb: { type: String },
+    ac_cable_accb_transformer: { type: String },
+    ac_ht_cable_11KV: { type: String,default:" " },
+    ac_ht_cable_33KV: { type: String,default:" " },
+    earthing_station: { type: String },
+    earthing_strips: { type: String },
+    earthing_strip: { type: String },
+    lightening_arrestor: { type: String },
+    datalogger: { type: String },
+    auxilary_transformer: { type: String },
+    ups_ldb: { type: String },
+    balance_of_system: { type: String },
+    transportation: { type: String },
+    transmission_line_11kv: { type: String,default:" " },
+    transmission_line_33kv: { type: String,default:" "},
+    // transmission_line_internal: { type: String },
+    // transmission_line_print: { type: String },
+    ct_pt_11kv_MP: { type: String,default:" " },
+    ct_pt_33kv_MP: { type: String,default:" " },
+    ct_pt_11kv_Other: { type: String,default:" " },
+    ct_pt_33kv_Other: { type: String,default:" " },
+     abt_meter_11kv_MP: { type: String,default:" " },
+     abt_meter_33kv_MP: { type: String,default:" " },
+     abt_meter_11kv_Other: { type: String,default:" " },
+     abt_meter_33kv_Other: { type: String,default:" " },
+    
+    vcb_kiosk: { type: String },
+    slnko_charges: { type: String },
+    installation_commissioing: {
+      labour_works: { type: String },
+      machinery: { type: String },
+      civil_material: { type: String },
+    },
+    // labour_works: { type: String },
+    // machinery: { type: String },
+    // civil_material: { type: String },
+    submitted_by: { type: String },
+
+
+ },{timestamps:true});
+
+ module.exports = mongoose.model('commBDRate', commBDRateSchema);
