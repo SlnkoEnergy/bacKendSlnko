e80304163220309579c029b3e6f0c10603616c87
Biplove82 <biplavmandal.mandal@gmail.com>
2025-02-15 15:43:47 +0530

    git push routes

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -105,55 +105,55 @@ const{ addCommBDRate, editCommBDRate, getCommBDRate, deleteCommBDRate }=require(
 
 
 // Admin router
-router.post("/user-registratioN", userRegister);
-router.post("/logiN", login);
-router.get("/get-all-useR", getalluser);
-router.post("/forget-password-send-otP", forgettpass);
-router.post("/received-emaiL", verifyandSendPass);
-router.delete("/delete-useR/:_id", deleteUser);
-router.get("/get-single-useR/:_id", getSingleUser);
+router.post("/user-registratioN-IT", userRegister);
+router.post("/logiN-IT", login);
+router.get("/get-all-useR-IT", getalluser);
+router.post("/forget-password-send-otP-IT", forgettpass);
+router.post("/received-emaiL-IT", verifyandSendPass);
+router.delete("/delete-useR-IT/:_id", deleteUser);
+router.get("/get-single-useR-IT/:_id", getSingleUser);
 
 //project router
-router.post("/add-new-projecT", createProject);
-router.put("/update-projecT/:_id", updateProject);
-router.get("/get-all-projecT", getallproject);
-router.delete("/delete-by-iD/:_id", deleteProjectById); //delete project by id
-router.get("/get-project-iD/:_id", getProjectById); //get project by id
+router.post("/add-new-projecT-IT", createProject);
+router.put("/update-projecT-IT/:_id", updateProject);
+router.get("/get-all-projecT-IT", getallproject);
+router.delete("/delete-by-iD-IT/:_id", deleteProjectById); //delete project by id
+router.get("/get-project-iD-IT/:_id", getProjectById); //get project by id
 
 //addMoney APi
-router.post("/Add-MoneY", addMoney);
-router.get("/all-bilL", allbill);
-router.post("/get-bilL", credit_amount);
+router.post("/Add-MoneY-IT", addMoney);
+router.get("/all-bilL-IT", allbill);
+router.post("/get-bilL-IT", credit_amount);
 router.delete("/delete-crdit-amount/:_id",deleteCreditAmount);
 
 
 //purchase order controller
-router.post("/Add-purchase-ordeR", addPo);
-router.put("/edit-pO/:_id", editPO);
-router.get("/get-pO/:_id", getPO);
-router.get("/get-all-pO", getallpo);
+router.post("/Add-purchase-ordeR-IT", addPo);
+router.put("/edit-pO-IT/:_id", editPO);
+router.get("/get-pO-IT/:_id", getPO);
+router.get("/get-all-pO-IT", getallpo);
 router.post("/export-to-csv", exportCSV);
 router.put("/remove-to-recovery/:_id", moverecovery);
 router.get("/get-po-by-p_id/", getPOByProjectId);
-router.delete("/delete-pO/:_id",deletePO);
-router.get ("/get-po-historY",getpohistory);
+router.delete("/delete-pO-IT/:_id",deletePO);
+router.get ("/get-po-historY-IT",getpohistory);
 router.get("/get-po-history-iD/:_id", getPOHistoryById);
 
 //Add vendor
-router.post("/Add-vendoR", addVendor);
-router.get("/get-all-vendoR", getVendor);
-router.put("/update-vendoR/:_id", updateVendor); //update vendor
-router.delete("/delete-vendoR/:_id", deleteVendor); //delete vendor
+router.post("/Add-vendoR-IT", addVendor);
+router.get("/get-all-vendoR-IT", getVendor);
+router.put("/update-vendoR-IT/:_id", updateVendor); //update vendor
+router.delete("/delete-vendoR-IT/:_id", deleteVendor); //delete vendor
 
 //item
-router.post("/add-iteM", additem);
-router.get("/get-iteM", getItem);
+router.post("/add-iteM-IT", additem);
+router.get("/get-iteM-IT", getItem);
 
 //pay Request api
-router.get("/get-pay-sumrY",getPay);
-router.post("/add-pay-requesT", payRrequest);
-router.post("/hold-paymenT", holdpay);
-router.get("/get-pay-summarY", getPaySummary);
+router.get("/get-pay-sumrY-IT",getPay);
+router.post("/add-pay-requesT-IT", payRrequest);
+router.post("/hold-paymenT-IT", holdpay);
+router.get("/get-pay-summarY-IT", getPaySummary);
 router.get("/hold-pay-summary-IT", hold);
 router.put("/acc-matched", account_matched);
 router.put("/utr-update", utrUpdate);
@@ -172,16 +172,16 @@ router.get("/get-single-excel-data/:_id", getExcelDataById);
 router.get("/get-pay-smry",getpy);
 
 // add-Bill
-router.post("/add-bilL", addBill);
-router.get("/get-all-bilL", getBill);
+router.post("/add-bilL-IT", addBill);
+router.get("/get-all-bilL-IT", getBill);
 router.put("/update-bill/:_id", updatebill);
 router.delete("/delete-credit-amount/:_id", deletecredit);
 router.delete("/delete-bill/:_id", deleteBill);
 router.put("/accepted-by",bill_approved);
 
 //subtractmoney-debitmoney
-router.post("/debit-moneY", subtractmoney);
-router.get("/get-subtract-amounT", getsubtractMoney);
+router.post("/debit-moneY-IT", subtractmoney);
+router.get("/get-subtract-amounT-IT", getsubtractMoney);
 router.delete("/delete-debit-money/:_id", deleteDebitMoney);
 router.put("/recovery-debit/:_id", recoveryDebit);//to test for rrecovery subtract money
 router.delete("/delete-subtract-moneY/:_id", deleteSubtractMoney);
