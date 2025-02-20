62fa7699a6fe1850dc220e55db16906ecb40660b
Biplove82 <biplavmandal.mandal@gmail.com>
2025-02-20 16:06:01 +0530

    scm rate log

--- src/Controllers/commScmRateController.js
+++ src/Controllers/commScmRateController.js
@@ -1,4 +1,5 @@
 const commScmRateModells = require("../Modells/commScmRateModells");
+const commScmRateHistoryModells = require("../Modells/commScmRateHistoryModells");  
 
 //add commScmRate
 const addCommScmRate = async function (req, res) {
@@ -117,6 +118,49 @@ const editCommScmRate = async function (req, res) {
     if (!data) {
       return res.status(404).json({ msg: "user not found" });
     }
+    let commScmRateHistory = new commScmRateHistoryModells({
+      spv_modules_555: data.spv_modules_555,
+      spv_modules_580: data.spv_modules_580,
+      spv_modules_550: data.spv_modules_550,
+      spv_modules_585: data.spv_modules_585,
+      solar_inverter: data.solar_inverter,
+      module_mounting_structure: data.module_mounting_structure,
+      mounting_hardware: data.mounting_hardware,
+      dc_cable: data.dc_cable,
+      ac_cable_inverter_accb: data.ac_cable_inverter_accb,
+      ac_cable_accb_transformer: data.ac_cable_accb_transformer,
+      ac_ht_cable_11KV: data.ac_ht_cable_11KV,
+      ac_ht_cable_33KV: data.ac_ht_cable_33KV,
+      earthing_station: data.earthing_station,
+      earthing_strips: data.earthing_strips,
+      earthing_strip: data.earthing_strip,
+      lightening_arrestor: data.lightening_arrestor,
+      datalogger: data.datalogger,
+      auxilary_transformer: data.auxilary_transformer,
+      ups_ldb: data.ups_ldb,
+      balance_of_system: data.balance_of_system,
+      transportation: data.transportation,
+      transmission_line_11kv: data.transmission_line_11kv,
+      transmission_line_33kv: data.transmission_line_33kv,
+      ct_pt_11kv_MP: data.ct_pt_11kv_MP,
+      ct_pt_33kv_MP: data.ct_pt_33kv_MP,
+      ct_pt_11kv_Other: data.ct_pt_11kv_Other,
+      ct_pt_33kv_Other: data.ct_pt_33kv_Other,
+      abt_meter_11kv_MP: data.abt_meter_11kv_MP,
+      abt_meter_33kv_MP: data.abt_meter_33kv_MP,
+      abt_meter_11kv_Other: data.abt_meter_11kv_Other,
+      abt_meter_33kv_Other: data.abt_meter_33kv_Other,
+      vcb_kiosk: data.vcb_kiosk,
+      slnko_charges_scm: data.slnko_charges_scm,
+      installation_commissioing: {
+        labour_works: data.installation_commissioing.labour_works,
+        machinery: data.installation_commissioing.machinery,
+        civil_material: data.installation_commissioing.civil_material,
+      },
+      submitted_by_scm: data.submitted_by_scm,
+    });
+    await commScmRateHistory.save();
+
 
     return res
       .status(200)
