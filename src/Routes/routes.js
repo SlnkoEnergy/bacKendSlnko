5b362b69439d13b56aeaadfa49f0a70ec43f7de8
Biplove82 <biplavmandal.mandal@gmail.com>
2025-05-08 13:17:13 +0530

    add index and routes file

--- src/Routes/routes.js
+++ src/Routes/routes.js
@@ -237,6 +237,10 @@ const { create } = require("../Modells/bdleadsModells");
 
 const upload = require("../middlewares/multer.js");
 
+const{ createMaterial, getAllMaterials, deleteMaterial, updateMaterial }=require("../Controllers/engineeringController/materialController");
+
+const{ addMaterialCategory, getAllMaterialCategories, deleteMaterialCategory, updateMaterialCategory }=require("../Controllers/engineeringController/materialCategoryController");
+
 // Admin router
 router.post("/user-registratioN-IT", userRegister);
 router.post("/logiN-IT", login);
@@ -1196,5 +1200,16 @@ router.put(
   jwtMW.authorization,
   updateLeadStatus
 );
+//material master Engineering
+router.post("/add-material",createMaterial );
+router.get("/get-material",getAllMaterials );
+router.delete("/delete-mateial/:_id",deleteMaterial);
+router.put("/update-material/:_id",updateMaterial); //get all material categories
+
+//material category master Engineering
+router.post("/add-material-category",addMaterialCategory );
+router.get("/get-material-category", getAllMaterialCategories);
+router.put("/edit-material-category/:_id", updateMaterialCategory); //update material category
+router.delete("/delete-material-category/:_id", deleteMaterialCategory); //delete material category
 
 module.exports = router;
