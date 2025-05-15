945eef0bdcecdb61c67c2f5a7537e0934e2edc9f
whomohitgoyal <mohit@droot.in>
2025-05-15 16:18:58 +0530

    -- added keys via env

--- src/Authentication/auth.js
+++ src/Authentication/auth.js
@@ -4,7 +4,7 @@ const authentication = async function (req, res,next) {
     let token = req.headers["x-auth-token"];
     if (!token) return res.status(401).send({ status: false, msg: "Token must be present" });
   
-    jwt.verify(token, " your-secret-key", function (err, decodedToken) {
+    jwt.verify(token, process.env.PASSKEY, function (err, decodedToken) {
         if (err) return res.status(401).send({ status: false, msg: "Token is invalid" });
   
         req.user = decodedToken; // attach user data to request
@@ -22,7 +22,7 @@ const authentication = async function (req, res,next) {
     if (!token) token = req.headers["x-auth-token"];
     
     
-    let decodedToken = jwt.verify(token, " your-secret-key");
+    let decodedToken = jwt.verify(token, process.env.PASSKEY);
     if (decodedToken.userId !==req.params.userId){
       return res.send({ status: false, msg: "UserId or Token is Wrong" });
     }
