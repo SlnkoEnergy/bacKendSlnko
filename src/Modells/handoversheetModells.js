15ead79e86f9756c56b03e10c76f303ca91b3993
Biplove82 <biplavmandal.mandal@gmail.com>
2025-05-17 13:23:09 +0530

    hand oversheet  key update

--- src/Modells/handoversheetModells.js
+++ src/Modells/handoversheetModells.js
@@ -5,80 +5,80 @@ const handoversheetSchema = new mongoose.Schema(
     p_id: { type: Number },
 
     customer_details: {
-      code: { type: String, required: true },
+      code: { type: String },
       name: { type: String, required: true },
       customer: { type: String, default: " ", required: true },
       epc_developer: { type: String, required: true },
-      site_google_coordinates: { type: String, required: true },
+      site_google_coordinates: { type: String },
       number: { type: Number, required: true },
-      gst_no: { type: String, required: true },
+     
       gender_of_Loa_holder: { type: String, required: true },
       email: { type: String, required: true },
-      pan_no: { type: String, required: true },
+      pan_no: { type: String },
       adharNumber_of_loa_holder: { type: String, required: true },
-      alt_number: { type: Number, required: true },
-      p_group: { type: String, required: true },
+      alt_number: { type: Number },
+      p_group: { type: String },
 
-      billing_address: {
-        village_name: { type: String, required: true },
-        district_name: { type: String, required: true },
-      },
-
+     
       site_address: {
-        village_name: { type: String, required: true },
-        district_name: { type: String, required: true },
+        village_name: { type: String  },
+        district_name: { type: String  },
       },
 
       state: { type: String, required: true },
     },
 
     order_details: {
-      type_business: { type: String , required: true },
-      tender_name: { type: String , required: true },
+      type_business: { type: String, required: true },
+      tender_name: { type: String, required: true },
       discom_name: { type: String, required: true },
-      design_date: { type: String,  },
+      design_date: { type: String },
       feeder_code: { type: String, required: true },
       feeder_name: { type: String, required: true },
     },
 
     project_detail: {
-      project_component: { type: String, required: true, enum: ["KA", "KC", "KC2", "Other"] },
-      project_component_other: {  type: String,
-          required: function () {
-    return this.project_component === "Other";
-        } },
+      project_component: {
+        type: String,
+        required: true,
+       
+      },
+      project_component_other: {
+        type: String,
+       
+      },
       loan_scope: { type: String, required: true },
       project_type: { type: String, required: true },
-      module_make_capacity: { type: String,required: true },
-      module_make: { type: String, default: "", required: true },
-      module_capacity: { type: String, default: "", required: true },
-      module_type: { type: String, default: "", required: true },
-      module_model_no: { type: String, default: "", required: true },
+      module_make_capacity: { type: String, required: true },
+      module_make: { type: String,  required: true },
+      module_capacity: { type: String,  required: true },
+      module_type: { type: String,  required: true },
+      module_category: { type: String,  required: true },
       evacuation_voltage: { type: String, required: true },
-      inverter_make_capacity: { type: String,required: true },
-      inverter_make: { type: String, default: "", required: true },
-      inverter_type: { type: String, default: "", required: true },
-      inverter_size: { type: String, default: "", required: true },
-      inverter_model_no: { type: String, default: "", required: true },
+      inverter_make_capacity: { type: String, required: true },
+      inverter_make: { type: String,  required: true },
+      inverter_type: { type: String,  required: true },
+      inverter_size: { type: String,  required: true },
+      inverter_model_no: { type: String,  required: true },
       work_by_slnko: { type: String, required: true },
       topography_survey: { type: String, required: true },
       transmission_scope: { type: String, required: true },
       soil_test: { type: String, required: true },
       purchase_supply_net_meter: { type: String, required: true },
-      liaisoning_net_metering: { type: String,  required: true },
+      liaisoning_net_metering: { type: String, required: true },
       ceig_ceg: { type: String, required: true },
-      project_completion_date: { type: String, required: true },
+      project_completion_date: { type: String},
       proposed_dc_capacity: { type: String, required: true },
 
       project_kwp: { type: String, required: true },
       distance: { type: String, required: true },
-      tarrif: { type: String, required: true },
+      tarrif: { type: String },
       land: { type: String, required: true },
 
-      substation_name: { type: String, required: true },
+    
       overloading: { type: String, required: true },
 
-      agreement_date: { type: String, required: true },
+      agreement_date: { type: String },
     },
 
     commercial_details: {
@@ -88,16 +88,16 @@ const handoversheetSchema = new mongoose.Schema(
 
     other_details: {
       taken_over_by: { type: String, required: true },
-      cam_member_name: { type: String, required: true },
-      loa_number: { type: String, required: true },
-      ppa_number: { type: String, required: true },
+      cam_member_name: { type: String},
+      loa_number: { type: String },
+      ppa_number: { type: String },
       submitted_by_BD: { type: String },
       service: { type: String },
       billing_type: {
         type: String,
       },
       project_status: { type: String },
-      remark:{ type: String,required: true },
+      remark: { type: String, required: true },
     },
     invoice_detail: {
       invoice_recipient: { type: String },
