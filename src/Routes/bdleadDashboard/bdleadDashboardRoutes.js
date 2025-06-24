d2c973faee9cb9c3b028e28fb7916f1a67cbd2c7
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-06-24 15:10:50 +0530

    added get task by leadId

--- src/Routes/bdleadDashboard/bdleadDashboardRoutes.js
+++ src/Routes/bdleadDashboard/bdleadDashboardRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const {  getAllLeads, getLeadSummary, getLeadSource, taskDashboard, leadSummary,  leadconversationrate, getLeadByLeadIdorId, leadWonAndLost, leadFunnel, getAllLeadDropdown }=require("../../Controllers/bdController/bdleadDashboard");
 const { getNotesById, createNotes, updateNotes, deleteNotes, getNotesByLeadId } = require("../../Controllers/bdController/notesController");
-const { getTaskById, createTask, updateTask, deleteTask, updateStatus, getAllTask } = require("../../Controllers/bdController/taskController");
+const { getTaskById, createTask, updateTask, deleteTask, updateStatus, getAllTask, getTaskByLeadId } = require("../../Controllers/bdController/taskController");
 const jwtMW = require("../../middlewares/auth");
 // Bd lead Dashboard Routes
 router.get("/summary",jwtMW.authentication, jwtMW.authorization,getLeadSummary);
@@ -24,6 +24,7 @@ router.put('/bd-tasks/:_id', jwtMW.authentication, jwtMW.authorization,updateTas
 router.delete('/bd-tasks/:_id',jwtMW.authentication, jwtMW.authorization, deleteTask);      
 router.put('/:_id/updateStatus', jwtMW.authentication, jwtMW.authorization, updateStatus)
 router.get('/all-tasks', jwtMW.authentication, jwtMW.authorization, getAllTask);
+router.get('/bd-tasks', jwtMW.authentication, jwtMW.authorization, getTaskByLeadId);
 
 //Notes Routes
 router.get('/bd-notes/:_id',jwtMW.authentication, jwtMW.authorization, getNotesById);        
