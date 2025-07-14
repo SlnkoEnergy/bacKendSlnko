71bc5c055de059694d397d7a56bfe6adbe311399
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-07-14 20:32:50 +0530

    added update Status APi

--- src/Routes/tasks/tasks.js
+++ src/Routes/tasks/tasks.js
@@ -1,6 +1,6 @@
 var router = require("express").Router();
 
-const { createTask, getAllTasks, updateTask, getTaskById, deleteTask }=require("../../Controllers/tasksController/tasksControllers");
+const { createTask, getAllTasks, updateTask, getTaskById, deleteTask, updateTaskStatus }=require("../../Controllers/tasksController/tasksControllers");
 const jwtMW = require("../../middlewares/auth");
 
 router.post('/create-task',jwtMW.authentication, jwtMW.authorization, createTask);
@@ -8,4 +8,5 @@ router.get('/get-task', jwtMW.authentication, jwtMW.authorization, getAllTasks);
 router.get('/get-task/:id', jwtMW.authentication, jwtMW.authorization, getTaskById);
 router.put('/update-task/:id', jwtMW.authentication, jwtMW.authorization, updateTask); 
 router.delete('/delete-task/:id',jwtMW.authentication, jwtMW.authorization, deleteTask );
+router.put('/:id/updateTaskStatus', jwtMW.authentication, jwtMW.authorization,updateTaskStatus);
 module.exports = router;
