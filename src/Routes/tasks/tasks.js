bf12ae60ad04535fe452593fbc7c23c80324341d
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-07-14 16:23:25 +0530

    added auth in task api

--- src/Routes/tasks/tasks.js
+++ src/Routes/tasks/tasks.js
@@ -1,10 +1,11 @@
 var router = require("express").Router();
 
 const { createTask, getAllTasks, updateTask, getTaskById, deleteTask }=require("../../Controllers/tasksController/tasksControllers");
+const jwtMW = require("../../middlewares/auth");
 
-router.post('/create-task',createTask);
-router.get('/get-task', getAllTasks); 
-router.get('/get-task/:id', getTaskById);
-router.put('/update-task/:id', updateTask); 
-router.delete('/delete-task/:id',deleteTask );
+router.post('/create-task',jwtMW.authentication, jwtMW.authorization, createTask);
+router.get('/get-task', jwtMW.authentication, jwtMW.authorization, getAllTasks); 
+router.get('/get-task/:id', jwtMW.authentication, jwtMW.authorization, getTaskById);
+router.put('/update-task/:id', jwtMW.authentication, jwtMW.authorization, updateTask); 
+router.delete('/delete-task/:id',jwtMW.authentication, jwtMW.authorization, deleteTask );
 module.exports = router;
