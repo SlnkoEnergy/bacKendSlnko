f195c75a2e798ebf5e7a113bf3a13152e2cde11b
Biplove82 <biplavmandal.mandal@gmail.com>
2025-07-21 18:39:32 +0530

    coustmomer pay sumary debit credit done only adj left

--- src/Routes/Accounting/accountingRoutes.js
+++ src/Routes/Accounting/accountingRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { paymentApproved, utrSubmission }=require("../../Controllers/Accounting/approvedPayment");
-const { getCustomerPaymentSummary } = require("../../Controllers/Accounting/customerpaymentSummary");
+const { getCustomerPaymentSummary, clientHistory } = require("../../Controllers/Accounting/customerpaymentSummary");
 const { paymentApproval } = require("../../Controllers/Accounting/paymentApproval");
 const { projectBalance }= require("../../Controllers/Accounting/ProjectBalance");
 const { standbyRecord } = require("../../Controllers/Accounting/standbyRecord");
@@ -12,5 +12,6 @@ router.get("/project-balance",projectBalance);
 router.get("/payment-approval",paymentApproval);
 router.get("/standby-record",standbyRecord);
 router.get("/customer-payment-summary",getCustomerPaymentSummary);
+router.get("/client-history",clientHistory)
  
 module.exports = router;
