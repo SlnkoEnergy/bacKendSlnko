f28b37aa17ec5fd0b6c31d8a8c33cd384cc0fec4
Biplove82 <biplavmandal.mandal@gmail.com>
2025-07-23 19:07:23 +0530

    seperate api for credit debit and adjustment

--- src/Routes/Accounting/accountingRoutes.js
+++ src/Routes/Accounting/accountingRoutes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { paymentApproved, utrSubmission }=require("../../Controllers/Accounting/approvedPayment");
-const { getCustomerPaymentSummary, clientHistory, totalBalanceSummary } = require("../../Controllers/Accounting/customerpaymentSummary");
+const { getCustomerPaymentSummary, clientHistory, totalBalanceSummary, getCreditSummary, getDebitSummary, getAdjustmentHistory } = require("../../Controllers/Accounting/customerpaymentSummary");
 const { paymentApproval } = require("../../Controllers/Accounting/paymentApproval");
 const { projectBalance , exportProjectBalance  }= require("../../Controllers/Accounting/ProjectBalance");
 const { standbyRecord } = require("../../Controllers/Accounting/standbyRecord");
@@ -14,5 +14,8 @@ router.get("/payment-approval",paymentApproval);
 router.get("/standby-record",standbyRecord);
 router.get("/customer-payment-summary",getCustomerPaymentSummary);
 router.get("/client-history",clientHistory);
-router.get("/balance-summary",totalBalanceSummary) 
+router.get("/balance-summary",totalBalanceSummary) ;
+router.get("/credit-summary",getCreditSummary);
+router.get("/debit-summary",getDebitSummary);
+router.get("/adjustment-history",getAdjustmentHistory);
 module.exports = router;
