2c454388204e61d6ecc6dee966564b437e188bf0
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-07-28 03:46:21 +0530

    changed system identifier

--- src/utils/generateSystemIdentifier.js
+++ src/utils/generateSystemIdentifier.js
@@ -1,20 +1,16 @@
-const os = require("os");
-const axios = require("axios");
 
 async function getSystemIdentifier() {
-  const hostname = os.hostname();
-  const platform = os.platform();
-  const arch = os.arch();
-  const cpus = os.cpus().map(cpu => cpu.model).join("-");
-  const interfaces = Object.values(os.networkInterfaces()).flat();
-   const baseBoard = os.userInfo().username;
+   let device_id = req.cookies.device_id;
+  if (!device_id) {
+    device_id = uuidv4();
+    res.cookie("device_id", device_id, { httpOnly: true, maxAge: 31536000000 });
+  }
+  
   const externalIfaces = interfaces.filter(
     iface => !iface.internal && iface.mac !== "00:00:00:00:00:00"
   );
 
   const ip = externalIfaces.map(iface => iface.address).join("-");
-
-  const device_id = `${hostname}-${platform}-${arch}-${cpus}-${baseBoard}`;
 
   return {
     device_id,
