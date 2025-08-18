fb1911669d3828d7eb7a87e6b6d977828d620930
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-08-18 20:01:05 +0530

    added code

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ const cookieParser = require("cookie-parser");
 const Sentry = require("@sentry/node");
 const Tracing = require("@sentry/tracing");
 require("../src/utils/cron/inactivelead.cron.utils");
+require("../src/utils/cron/movetotrash.cron.utils");
 
 Sentry.init({
   dsn: "https://50b42b515673cd9e4c304951d05cdc44@o4509774671511552.ingest.us.sentry.io/4509774818508800",
@@ -59,7 +60,6 @@ const startServer = async () => {
     await mongoose.connect(db, {});
     console.log("SlnkoEnergy database is connected");
 
-    require("../src/utils/cron/movetotrash.cron.utils");
 
     app.use("/v1", routes);
     app.use("/v1/engineering", engineeringRoutes);
