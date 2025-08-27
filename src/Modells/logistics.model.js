510d81a72301f6a0a5d463b034b7879a65aab8db
KumarRounik <rounikpandey@gmail.com>
2025-08-27 14:54:36 +0530

    code done

--- src/Modells/logistics.model.js
+++ src/Modells/logistics.model.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const updateStatus = require("../utils/updatestatus.utils");
 const logisticSchema = new mongoose.Schema(
   {
     po_id: [
@@ -73,5 +73,9 @@ const logisticSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+logisticSchema.pre("save", function (next) {
+  updateStatus(this, "ready_to_dispatch"); 
+  next();                                   
+});
 
 module.exports = mongoose.model("Logistic", logisticSchema);
