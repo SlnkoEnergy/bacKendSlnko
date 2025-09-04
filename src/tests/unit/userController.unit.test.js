1adcad8dfe44411bcd49a6c208d0abb0f406e85b
Sarvesh-shrimal <sarveshshrimal2409@gmail.com>
2025-09-04 15:35:47 +0530

    added code

--- src/tests/unit/userController.unit.test.js
+++ src/tests/unit/userController.unit.test.js
@@ -1,4 +1,4 @@
-const jest = require("jest");
+// const jest = require("jest");
 const supertest = require("supertest");
 const app = require("../../index");
 const bcrypt = require("bcrypt");
@@ -7,6 +7,25 @@ const userModells = require("../../Modells/users/userModells");
 
 jest.mock("bcrypt")
 jest.mock("jsonwebtoken");
-jest.mock("../../middlewares/auth", () => {
-    return(res, req, next) => next();
+// jest.mock("../../middlewares/auth", () => {
+//     return(res, req, next) => next();
+// });
+
+describe("UserRegister Testing with bcrypt", () => {
+    beforeAll(() => {
+        process.env.PASSKEY = "testscrete"
+    }) 
+
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    test("should return 400 if required fields are missing", async() => {
+        const res = await supertest(app)
+        .post("/v1/user-registratioN-IT")
+        .send({name: '', emp_id : '', email: '', password : ''});
+
+        expect(res.status).toBe(400);
+        expect(res.body.msg).toMatch("All fields are required");
+    });
 });
