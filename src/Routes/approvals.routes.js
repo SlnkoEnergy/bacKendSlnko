22152fee601972ca84e43f604b7e81514cd25efb
KumarRounik <rounikpandey@gmail.com>
2025-09-29 19:15:38 +0530

    made changes in the api of the updatedependency

--- src/Routes/approvals.routes.js
+++ src/Routes/approvals.routes.js
@@ -5,10 +5,12 @@ const {
   updateStatus,
   getAllRequests,
   getAllReviews,
+  getApprovalFormById,
 } = require("../Controllers/approvals.controller");
 const jwtMW = require("../middlewares/auth");
 
 router.post("/approval", jwtMW.authentication, createApproval);
+router.get("/approvals/:id", jwtMW.authentication, getApprovalFormById);
 router.get("/uniquemodels", jwtMW.authentication, getUniqueApprovalModels);
 router.put("/:approvalId/updateStatus", jwtMW.authentication, updateStatus);
 router.get("/requests", jwtMW.authentication, getAllRequests);
