255473b43c1dce10d5f8bc65f64182719f3927a3
sarvesh-shrimal <sarveshshrimal2409@gmail.com>
2025-10-26 13:01:00 +0530

    added code for commitment date scope

--- src/models/scope.model.js
+++ src/models/scope.model.js
@@ -1,49 +1,102 @@
-const mongoose = require("mongoose");
-const updateStatus = require("../utils/updatestatus.utils");
+  const mongoose = require("mongoose");
+  const updateStatus = require("../utils/updatestatus.utils");
+const updateCommitmentDate = require("../utils/updatecommitmentdate.utils");
 
-const scopeSchema = new mongoose.Schema(
-  {
-    project_id: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "projectDetail",
-    },
-    items: [
-      {
-        item_id: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "MaterialCategory",
-        },
-        name: {
-          type: String,
-        },
-        type: {
-          type: String,
-          enum: ["supply", "execution"],
-        },
-        category: {
-          type: String,
-        },
-        scope: {
-          type: String,
-          enum: ["slnko", "client"],
-        },
-        quantity: {
-          type: String,
-        },
-        uom: {
-          type: String,
-        },
-        order: {
-          type: Number,
-        },
-        pr_status: {
-          type: Boolean,
-          default: false,
-        },
+  const scopeSchema = new mongoose.Schema(
+    {
+      project_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "projectDetail",
       },
-    ],
-    status_history: [
-      {
+      items: [
+        {
+          item_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "MaterialCategory",
+          },
+          name: {
+            type: String,
+          },
+          type: {
+            type: String,
+            enum: ["supply", "execution"],
+          },
+          category: {
+            type: String,
+          },
+          scope: {
+            type: String,
+            enum: ["slnko", "client"],
+          },
+          quantity: {
+            type: String,
+          },
+          uom: {
+            type: String,
+          },
+          order: {
+            type: Number,
+          },
+          pr_status: {
+            type: Boolean,
+            default: false,
+          },
+          commitment_date_history: [
+            {
+              date: {
+                type: Date,
+              },
+              remarks: {
+                type: String,
+              },
+              user_id: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "User",
+              },
+              updatedAt: {
+                type: Date,
+                default: Date.now(),
+              },
+            },
+          ],
+          current_commitment_date: {
+            date: {
+              type: Date,
+            },
+            remarks: {
+              type: String,
+            },
+            user_id: {
+              type: mongoose.Schema.Types.ObjectId,
+              ref: "User",
+            },
+            updatedAt: {
+              type: Date,
+              default: Date.now(),
+            },
+          },
+        },
+      ],
+      status_history: [
+        {
+          status: {
+            type: String,
+            enum: ["open", "closed"],
+          },
+          remarks: {
+            type: String,
+          },
+          user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+          },
+          updatedAt: {
+            type: Date,
+            default: Date.now,
+          },
+        },
+      ],
+      current_status: {
         status: {
           type: String,
           enum: ["open", "closed"],
@@ -55,36 +108,19 @@ const scopeSchema = new mongoose.Schema(
           type: mongoose.Schema.Types.ObjectId,
           ref: "User",
         },
-        updatedAt: {
-          type: Date,
-          default: Date.now,
-        },
       },
-    ],
-    current_status: {
-      status: {
-        type: String,
-        enum: ["open", "closed"],
-      },
-      remarks: {
-        type: String,
-      },
-      user_id: {
+      createdBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
       },
     },
-    createdBy: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-    },
-  },
-  { timestamps: true }
-);
+    { timestamps: true }
+  );
 
-scopeSchema.pre("save", function (next) {
-  updateStatus(this, "open");
-  next();
-});
+  scopeSchema.pre("save", function (next) {
+    updateStatus(this, "open");
+    // updateCommitmentDate(this.items);
+    next();
+  });
 
-module.exports = mongoose.model("Scope", scopeSchema);
+  module.exports = mongoose.model("Scope", scopeSchema);
