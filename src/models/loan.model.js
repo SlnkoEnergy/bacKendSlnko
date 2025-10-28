ee1fe05c21528f734ff2741e1e66aaf26ef2e35d
Siddharth Singh <singhsiddharth1102@gmail.com>
2025-10-28 17:27:41 +0530

    added code for loan

---
+++ src/models/loan.model.js
@@ -0,0 +1,96 @@
+const mongoose = require("mongoose");
+const updateStatus = require("../utils/updatestatus.utils");
+
+const loanSchema = new mongoose.Schema({
+  project_id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "projectDetail",
+  },
+  documents: [
+    {
+      name: {
+        type: String,
+      },
+      attachment_url: {
+        type: String,
+      },
+      remarks: {
+        type: String,
+      },
+    },
+  ],
+  banking_details: [
+    {
+      name: {
+        type: String,
+      },
+      branch: {
+        type: String,
+      },
+      ifsc_code: {
+        type: String,
+      },
+    },
+  ],
+  status_history: [
+    {
+      status: {
+        type: String,
+        enum: [
+          "document pending",
+          "under process bank",
+          "sanctioned",
+          "disbursed",
+        ],
+      },
+      remarks: {
+        type: String,
+      },
+      user_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+      updatedAt: {
+        type: Date,
+        default: Date.now(),
+      },
+    },
+  ],
+  current_status: {
+    status: {
+      type: String,
+    },
+    remarks: {
+      type: String,
+    },
+    user_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now(),
+    },
+  },
+  timelines: {
+    expected_disbursement_date: {
+      type: Date,
+    },
+    expected_sanctioned_date: {
+      type: Date,
+    },
+    actual_disbursement_date: {
+      type: Date,
+    },
+    actual_sanctioned_date: {
+      type: Date,
+    },
+  },
+});
+
+loanSchema.pre("save", function (next) {
+  updateStatus(this, "document pending");
+  next();
+});
+
+module.exports = mongoose.model("loan", loanSchema);
